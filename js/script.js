f0f4c0a80f18050fa83072a5627ec22c37b7f0d9
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-04 14:28:05 -0500

    Updated displayCat function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-const randomNumber = document.querySelectorAll(" .random-number");
-let numberArray = [];
-let random = document.querySelector(".random");
+const catSection = document.querySelector(".cat-section");
+const button = document.querySelector("button");
 
-const numberGenerator = async function () {
+const catGenerator = async function () {
   const res = await fetch("https://cataas.com/cat");
   const cats = res.json();
+  randomCat(cats);
 
 }
 
 const randomCat = function(cats) {
   const randmIndex = Math.floor(Math.random() * cats.length);
   const catImage = cats[randmIndex];
+  displayCat(catImage);
 }
 
 const displayCat = function(catImage) {
-  
+  catSection.append(catImage);
 }
+
+button.addEventListener("click", function() {
+  catGenerator();
+
+})
 
 
 
