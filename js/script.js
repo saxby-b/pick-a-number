0f54ffa79a08ef5d6e6ccc89e5bf3a5d70ce675e
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-04 14:44:34 -0500

    Formatted code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,23 @@
-const catSection = document.querySelector(".cat-section");
+const img = document.querySelector("img");
 const button = document.querySelector("button");
 
 const catGenerator = async function () {
-  const res = await fetch("https://cataas.com/cat");
+  const res = await fetch("https://cataas.com/cat?json=true");
   const cats = res.json();
   randomCat(cats);
+};
 
-}
-
-const randomCat = function(cats) {
+const randomCat = function (cats) {
   const randmIndex = Math.floor(Math.random() * cats.length);
   const catImage = cats[randmIndex];
   displayCat(catImage);
-}
+};
 
-const displayCat = function(catImage) {
-  catSection.append(catImage);
-}
+const displayCat = function (catImage) {
+  const imgAddress = catImage.url;
+  img.src = imgAddress;
+};
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   catGenerator();
-
-})
-
-
-
-
-
-
-
-
-
+});
