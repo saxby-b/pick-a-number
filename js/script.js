84095fc5d4b28ce091728d19614b7f5742ac646e
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-05 14:05:01 -0500

    Updated displayCat function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 const img = document.querySelector("img");
 const button = document.querySelector("button");
 
-const catGenerator = async function () {
+/*const catGenerator = async function () {
   const res = await fetch("https://cataas.com/cat?json=true");
   const cats = res.json();
   displayCat(cats);
 };
 
-/*const randomCat = function (cats) {
+const randomCat = function (cats) {
   const randmIndex = Math.floor(Math.random() * cats.length);
   const catImage = cats[randmIndex];
   displayCat(catImage);
 }; */
 
-const displayCat = function (cats) {
-  const imgAddress = cats.url;
-  img.src = imgAddress;
+const displayCat = function () {
+  location.reload();
 };
 
-button.addEventListener("click", function () {
-  catGenerator();
-});
+button.addEventListener("click", displayCat
+);
