09a149be44f7730deb3b978e64ddf9c5706086c2
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-05 14:06:26 -0500

    Deleted comments and formatted code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
 const img = document.querySelector("img");
 const button = document.querySelector("button");
 
-/*const catGenerator = async function () {
-  const res = await fetch("https://cataas.com/cat?json=true");
-  const cats = res.json();
-  displayCat(cats);
-};
-
-const randomCat = function (cats) {
-  const randmIndex = Math.floor(Math.random() * cats.length);
-  const catImage = cats[randmIndex];
-  displayCat(catImage);
-}; */
-
 const displayCat = function () {
   location.reload();
 };
 
-button.addEventListener("click", displayCat
-);
+button.addEventListener("click", displayCat);
